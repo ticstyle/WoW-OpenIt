7d11ef40475c3cce573e70d410a0d55525ba3f28
dualityps <4126225+ticstyle@users.noreply.github.com>
2026-08-11 14:49:53 +0200

    Update hardcoded blacklist with additional item IDs

--- Data/Blacklist.lua
+++ Data/Blacklist.lua
@@ -5,17 +5,15 @@
 
 local _, addon = ...
 
--- Permanent blacklist for items that should never trigger the addon (Hearthstones, teleports, etc.)
--- These will NOT show up in the user options panel blacklist.
 addon.hardcodedBlacklist = {
-    -- Hearthstones & Teleport Toys/Items
     [6948] = true, -- Hearthstone
     [110560] = true, -- Garrison Hearthstone
     [140192] = true, -- Dalaran Hearthstone
     [128353] = true, -- Admiral's Compass
     [141605] = true, -- Flight Master's Whistle
     [200613] = true, -- Wyrmhole Generator
-
-    -- Add permanent blacklisted item IDs here:
-    -- [itemID] = true,
+    [184504] = true,
+    [140749] = true,
+    [246492] = true,
+    
 }
