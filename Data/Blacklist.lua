d98743bc37240f953ec771fe31b90013cbeeeedc
ticstyle <4126225+ticstyle@users.noreply.github.com>
2026-08-11 13:47:16 +0000

    style: automated code formatting (StyLua)

--- Data/Blacklist.lua
+++ Data/Blacklist.lua
@@ -6,14 +6,13 @@
 local _, addon = ...
 
 addon.hardcodedBlacklist = {
-    [6948] = true, -- Hearthstone
-    [110560] = true, -- Garrison Hearthstone
-    [140192] = true, -- Dalaran Hearthstone
-    [128353] = true, -- Admiral's Compass
-    [141605] = true, -- Flight Master's Whistle
-    [200613] = true, -- Wyrmhole Generator
-    [184504] = true,
-    [140749] = true,
-    [246492] = true,
-    
+	[6948] = true, -- Hearthstone
+	[110560] = true, -- Garrison Hearthstone
+	[140192] = true, -- Dalaran Hearthstone
+	[128353] = true, -- Admiral's Compass
+	[141605] = true, -- Flight Master's Whistle
+	[200613] = true, -- Wyrmhole Generator
+	[184504] = true,
+	[140749] = true,
+	[246492] = true,
 }
